2afcf228823aa5106bea19fb080371f6f7ce16f3
Diego-Cano <64940291+Diego-Cano@users.noreply.github.com>
2022-03-14 14:27:25 -0700

    Update news-view.php

--- rss/news-view.php
+++ rss/news-view.php
@@ -14,14 +14,52 @@ $config->metaKeywords = 'RSS News-view,PHP,'. $config->metaKeywords;
 //adds font awesome icons for arrows on pager
 $config->loadhead .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
 
+# check variable of item passed in - if invalid data, redirect back to index.php page
+if(isset($_GET['FeedsID']) && (int)$_GET['FeedsID'] > 0){#proper data must be on querystring
+    $myID = (int)$_GET['FeedsID']; #Convert to integer, will equate to zero if fails
+}else{
+   myRedirect(VIRTUAL_PATH . "rss/news.php"); //send the user back to a safe page
+}
+
+$sql = 'SELECT * FROM winter2022_rss_feeds WHERE FeedsID = '.$myID.'';
+$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
 get_header(); #defaults to theme header or header_inc.php
 ?>
 <h3 align="center">News View</h3>
 
-
 <?php
 
+if(mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result)){
+        $request = $row['FeedsUrl'];
+        $response = file_get_contents($request);
+        $xml = simplexml_load_string($response);
+    print '<h1>' . $xml->channel->title . '</h1>';
+
+           echo '<table class="table table-hover">' ;
+           echo '   <tr>
+                        <th scope="col" >Link</th>
+                        <th scope="col" >Source</th>
+                        <th scope="col" >Pubdate</th>
+                    </tr>';
+
+            foreach($xml->channel->item as $story)
+            {
+                echo '
+                <tr>
+                    <td><a href="' . $story->link  . '" target="_blank">' . $story->title . '</a></td>
+                    <td><a href="' . $story->source->attributes()->url  . '" target="_blank">' . $story->source . '</a></td>
+                    <td>' . $story->pubDate . '</td>
+                </tr>
+                '; 
+                
+                }
+
+            echo '</table>' ;
+
+}
+}
 
 
 //@mysqli_free_result($result);
